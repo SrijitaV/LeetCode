da9e781c3ba806e1fb549ff772974d0352781beb
Srijita Verma <srijitaverma06@gmail.com>
2026-07-10 17:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (41.67%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,16 +3,35 @@ public:
     int findDuplicate(vector<int>& nums) {
         
         //                                       brute force approach 
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == nums[i + 1])
-                return nums[i];
-        }
-        return 0;
+        // sort(nums.begin(), nums.end());
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     if (nums[i] == nums[i + 1])
+        //         return nums[i];
+        // }
+        // return 0;
+        //                                        TC = O(n logn)
+        //                                        SC = O(n)
 
+        //                                       negative flag 
+        for (int i = 0; i < nums.size(); i++) {
+
+            int index = abs(nums[i]);
+
+            if (nums[index] < 0) {
+                return index;
+            }
+
+            nums[index] *= -1;
+        }
+
+        return -1;
 
         //                                        bit manipulation 
         
+
+
+
+
     }
 };
