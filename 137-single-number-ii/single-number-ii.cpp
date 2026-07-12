2acc563d3e84ac2a9ffdd339d2ad96ca21f4b2d9
Srijita Verma <srijitaverma06@gmail.com>
2026-07-12 21:10:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (63.89%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
 // ------------------------------------------------------------------------------------
         //                                                                         map
-        map<int, int> mapp;
-        for (int i = 0; i < nums.size(); i++)
-            mapp[nums[i]]++;
-        for(auto it : mapp)
+        // map<int, int> mapp;
+        // for (int i = 0; i < nums.size(); i++)
+        //     mapp[nums[i]]++;
+        // for(auto it : mapp)
+        // {
+        //     if(it.second == 1)
+        //         return it.first;
+        // }
+        // return 0; 
+        //                                                  TC = O((n * logm) + m) + O(m)
+        //                                                  SC = O(m)
+
+    // }
+// ------------------------------------------------------------------------------------
+        //                                                                         counting bit position 
+        int ans = 0;
+        for(int i = 0; i < 32; i++)
         {
-            if(it.second == 1)
-                return it.first;
+            int count = 0; 
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if(nums[j] & (1 << i))
+                {
+                    count++;
+                }
+            }
+            if((count % 3) == 1)
+            {
+                ans |= (1 << i);
+            }
+            
         }
-        return 0; 
+        return ans;
     }
 };
