12dc29bc54881e114db1ce052a5b63c543f3368e
Srijita Verma <srijitaverma06@gmail.com>
2026-07-13 08:39:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.11%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // BRUTE FORCE 
+        double ans;
+        ans = pow(x, n);
+        return ans;
+        // TC = 
+        // SC = 
+
+        // RECURSION 
+
+        // TC = 
+        // SC = 
+    }
+};
