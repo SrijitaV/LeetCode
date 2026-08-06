0054a234d5c3a1a99d39a675903de7e3adff2e9e
Srijita Verma <srijitaverma06@gmail.com>
2026-08-06 15:13:57 +0530

    Time: 14 ms (22.58%) | Memory: 7.6 MB (89.14%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0 || n == 1)
-        {
             return n; 
-        }
-        else 
-        {
-            return (fib(n-1) + fib(n-2));
-        }
-        return -1;
+        return (fib(n-1) + fib(n-2));
     }
 };
