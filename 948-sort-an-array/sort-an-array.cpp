1f600f2fd5e7b9ee273d875695fa89e5225f6986
Srijita Verma <srijitaverma06@gmail.com>
2026-08-08 17:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 72.8 MB (69.69%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        // count how many times each number appears - tracking frequency 
+        // counting sort 
+
+        // max and min element 
         int mini = *min_element(nums.begin(), nums.end());
         int maxi = *max_element(nums.begin(), nums.end()); 
+                    // TC = O(n) + O(n) = O(n)
         vector<int> count(maxi - mini + 1, 0);
         for (int i = 0; i < nums.size(); i++)
             count[nums[i] - mini]++;
+                   // Time = O(k), Space = O(k)    here k is the range of values 
 
         // putting elemts into original array in sorted order 
         int ptr = 0; 
@@ -19,6 +23,7 @@ public:
                 count[i]--;
             }
         }
+                   //  Time = O(n), Space = O(1)
 
         return nums;
     }
