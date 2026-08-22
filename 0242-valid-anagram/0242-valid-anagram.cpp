47167e50dc4328af876626d6ac299f4fa899e85a
Srijita Verma <srijitaverma06@gmail.com>
2026-08-23 00:02:39 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
-            return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == t[i])
-                continue;
-            else
-                return false;
-        }
-        return true;
+        // basic sorting the strings and then comparing approach 
+        // if (s.size() != t.size())
+        //     return false;
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // for (int i = 0; i < s.size(); i++) {
+        //     if (s[i] == t[i])
+        //         continue;
+        //     else
+        //         return false;
+        // }
+        // return true;
+
+        // make frequency table for one increase then decrease and then check for zero
+        int freq[256] = {0};
+        for (int i = 0; i < s.size(); i++)
+            freq[s[i]]++;
+        for (int i = 0; i < t.size(); i++)
+            freq[t[i]]--; 
+        for (int i = 0; i < 256; i++)
+        {
+            if (freq[i] != 0)
+                return false; 
+        }      
+        return true; 
     }
 };
